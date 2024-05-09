b577a769507323692e19777d03e203ef3e15e02c
Darkmetal48 <luis.darkmetal.mena@gmail.com>
2024-05-08 22:48:58 -0400

    initial commit

---
+++ backend/database/migrations/2024_05_09_013447_create_employees_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_surname', 20);
+            $table->string('second_surname', 20);
+            $table->string('first_name', 20);
+            $table->string('middle_name', 50);
+
+            $table->string('job_country', 20);
+            $table->string('id_type', 20);
+
+            $table->string('id_number', 20)->unique();
+            $table->string('email', 255)->unique();
+            $table->date('date_of_entry');
+            $table->string('deparment', 30);
+            $table->integer('state')->default(1);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
